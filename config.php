e3f97e47571ce892838a953b00994c82aa2dd7b5
alf <sites@hubmed.org>
2010-01-05 16:21:31 +0000

    genericising the db name

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php 
-$theme = 'jqtouch/themes/apple';
-$title = 'Example WebApp';
-$app_db_name = 'org.hubmed.feedreader';
+$title = 'Example WebApp'; // edit this - the page title
+$app_db_name = 'com.example.webapp'; // edit this to something unique - it's the name of the client-side database
+$theme = 'jqtouch/themes/apple'; // the jQTouch theme to use
